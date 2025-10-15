ebff5d4edcb538812dbab4037ad1b6b161a6dca8
Ryszard Styczynski <rstyczynski@gmail.com>
2025-10-15 23:10:51 +0200

    test1.sh OK

--- test1.sh
+++ test1.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-ansible-playbook change_components_state.yml -e components="app1" -e state=RUNNING
-ansible-playbook change_components_state.yml -e components="node1" -e state=RUNNING
+set -e
 
-ansible-playbook change_components_state.yml -e components="node1" -e state=STOPPED
-ansible-playbook change_components_state.yml -e components="app1" -e state=STOPPED
+ansible-playbook change_state.yml -e component_name="app1" -e state=RUNNING
+ansible-playbook change_state.yml -e component_name="node1" -e state=RUNNING
 
-ansible-playbook change_components_state.yml -e components="app1,node1" -e state=RUNNING
+ansible-playbook change_state.yml -e component_name="node1" -e state=STOPPED
+ansible-playbook change_state.yml -e component_name="app1" -e state=STOPPED
+
+ansible-playbook change_state_bulk.yml -e components="app1,node1" -e state=RUNNING
+ansible-playbook change_state_bulk.yml -e components="app1,node1" -e state=STOPPED
+
+ansible-playbook node1_stop.yml
+ansible-playbook node1_start.yml
+
