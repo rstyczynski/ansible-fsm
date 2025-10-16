1bcb9bd88d6d7d613fdff00b67f0aed95d64f2a8
Ryszard Styczynski <rstyczynski@gmail.com>
2025-10-16 09:42:08 +0200

    app1 collection OK

---
+++ test_smoke.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Source the test framework
+source ./bin/test_helper.sh
+
+echo "app1: Remove state"
+mv ./state/state_app1.fact ./state/state_app1.fact.bak
+
+# Run tests
+run_test "app1: Start app1" "ansible-playbook change_state.yml -e component_name=app1 -e state=RUNNING"
+run_test "node1: Start node1" "ansible-playbook change_state.yml -e component_name=node1 -e state=RUNNING"
+
+echo "app1: Restore state"
+mv ./state/state_app1.fact.bak ./state/state_app1.fact
